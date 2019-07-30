b944b069ba5f4ff6006bd4c2fcec6bbe9a1f23ec
Shiyu Wang <shiyu3169@gmail.com>
2019-07-30 14:22:01 -0400

    be able to fetch req and get res from pixabay

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Search from "./components/search/Search";
+
 function App() {
   return (
     <MuiThemeProvider>
       <div>
         <Navbar />
+        <Search />
       </div>
     </MuiThemeProvider>
   );
---
+++ src/components/search/Search.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+
+export class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "13177801-5a69f408f31e818d9ae807abc",
+    images: []
+  };
+
+  onTextChange = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      },
+      () => {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res =>
+            this.setState({
+              images: res.data.hits
+            })
+          )
+          .catch(err => console.log(err));
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
+        />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+      </div>
+    );
+  }
+}
+
+export default Search;
